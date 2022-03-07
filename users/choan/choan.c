964c61c847068f0f7bfc7e61c9299963d07f3614
Choan Galvez <choan.galvez@gmail.com>
2022-03-07 19:18:27 +0100

    Better formatting

--- users/choan/choan.c
+++ users/choan/choan.c
@@ -2,16 +2,16 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT (
-		KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
-		HOME_A, HOME_S, HOME_D, HOME_F, HOME_G,                   HOME_H, HOME_J, HOME_K, HOME_L, HOME_QUO,
-		KC_Z,   X_ALTGR,   KC_C,   KC_V,   KC_B,   KC_NO,   KC_NO,   KC_N,   KC_M,   KC_COMM, DOT_ALTGR, KC_SLASH,
-		KC_NO,  KC_NO,  KC_NO,  MOU_NIL, NAV_SPC, NUM_TAB, KC_ENT, SYM_BCK, FUN_DEL, KC_VOLD, KC_VOLU, KC_MUTE
+		KC_Q,   KC_W,    KC_E,   KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,
+		HOME_A, HOME_S,  HOME_D, HOME_F,  HOME_G,                   HOME_H,  HOME_J,  HOME_K,  HOME_L,    HOME_QUO,
+		KC_Z,   X_ALTGR, KC_C,   KC_V,    KC_B,    KC_NO,   KC_NO,  KC_N,    KC_M,    KC_COMM, DOT_ALTGR, KC_SLASH,
+		KC_NO,  KC_NO,   KC_NO,  MOU_NIL, NAV_SPC, NUM_TAB, KC_ENT, SYM_BCK, FUN_DEL, KC_VOLD, KC_VOLU,   KC_MUTE
 	),
 	[_NAV]  = LAYOUT(
-		RESET,   KC_NO,    KC_NO,    KC_NO,    KC_NO,             U_RDO,    U_PST,   U_CPY,   U_CUT,   U_UND,
-		KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_HYPR,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS,
-		KC_NO,    KC_ALGR, KC_NO,    KC_NO,    KC_NO,     KC_NO, KC_NO,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
-		KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_ENT,  KC_BSPC, KC_DEL, KC_MPRV, KC_MPLY, KC_MNXT
+		RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                  U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
+		KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_HYPR,                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS,
+		KC_NO,   KC_ALGR, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
+		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_ENT, KC_BSPC, KC_DEL,  KC_MPRV, KC_MPLY, KC_MNXT
 		),
 	[_SYM]  = LAYOUT(
 		KC_TILD, KC_PLUS, KC_EQL,  KC_PERC, KC_HASH, KC_QUES, KC_RBRC, KC_LBRC, KC_AMPR, KC_CIRC,
@@ -32,6 +32,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_BTN1, KC_BTN3, KC_BTN2, KC_NO,    KC_NO , KC_NO
 
 		)
-
-
 };
